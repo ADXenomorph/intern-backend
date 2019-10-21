48da3a30a91c7ba13f962d37b161306f3f39d281
Kremnev Sergey <kremnev@roowix.com>
2019-10-21 17:22:13 +0700

    Updated dependencies and autoload

--- www/bootstrap.php
+++ www/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 register_shutdown_function(function () {
     $error = error_get_last();
     if ($error && $error['type'] === E_ERROR) {
@@ -9,10 +11,4 @@ register_shutdown_function(function () {
 
 set_exception_handler(function (\Exception $ex) {
     echo json_encode(['status' => 500, 'message' => $ex->getMessage(), 'payload' => []]);
-});
-
-spl_autoload_register(function ($className) {
-    $filename = __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
-
-    require $filename;
 });
