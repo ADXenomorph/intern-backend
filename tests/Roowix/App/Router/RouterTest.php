df81cd226c6581b3b884e9764ad85df99d81cb57
Kremnev Sergey <kremnev@roowix.com>
2019-10-28 03:24:25 +0700

    Added phpunit config

--- tests/Roowix/App/Router/RouterTest.php
+++ tests/Roowix/App/Router/RouterTest.php
@@ -1,11 +1,20 @@
 <?php
 
-namespace Roowix\Tests\App\
+namespace Roowix\Tests\App\Router;
 
 use PHPUnit\Framework\TestCase;
 use Roowix\App\Router\Router;
 
 class RouterTest extends TestCase
 {
+    public function testParse()
+    {
+        // arrange
+        $router = new Router(['/api/test' => 'sometestclass']);
 
+        // act
+        $res = $router->parse('/api/test');
+
+        // assert
+    }
 }
