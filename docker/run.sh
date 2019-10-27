2b0f464ea2ea64c0cd94c8af664e841030156ddd
Kremnev Sergey <kremnev@roowix.com>
2019-10-28 03:48:22 +0700

    Moved docker files to separate dir

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 # docker build -t internship .
 # docker stop intern && docker rm intern
 # docker build -t internship . && docker stop intern && docker rm intern && ./run.sh
-docker run -i -d -p 10080:80 -p 15432:5432 -v /home/xen/projects/internship/backend:/home/project --name intern internship
+docker run -i -d -p 10080:80 -p 15432:5432 -v $0/..:/home/project --name intern internship
