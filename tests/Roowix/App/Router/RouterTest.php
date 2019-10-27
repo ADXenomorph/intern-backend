7e933ea02231e0a5fbc10dc28017d788a6394570
Kremnev Sergey <kremnev@roowix.com>
2019-10-28 03:37:47 +0700

    Added simple router test

--- tests/Roowix/App/Router/RouterTest.php
+++ tests/Roowix/App/Router/RouterTest.php
@@ -7,14 +7,37 @@ use Roowix\App\Router\Router;
 
 class RouterTest extends TestCase
 {
-    public function testParse()
+    public function testParseSimpleRoute()
     {
         // arrange
-        $router = new Router(['/api/test' => 'sometestclass']);
+        $class = 'sometestclass';
+        $router = new Router(['/api/test' => $class]);
 
         // act
-        $res = $router->parse('/api/test');
+        $route = $router->parse('/api/test');
 
         // assert
+        $this->assertEquals($class, $route->getControllerClass());
+        $this->assertEmpty($route->getParams());
+    }
+
+    public function testParseParametrizedRoute()
+    {
+        // arrange
+        $class = 'sometestclass';
+        $router = new Router(['/api/test/{param}' => $class]);
+
+        // act
+        $stringRoute = $router->parse('/api/test/value');
+        $intRoute = $router->parse('/api/test/123');
+
+        // assert
+        $this->assertEquals($class, $stringRoute->getControllerClass());
+        $this->assertNotEmpty($stringRoute->getParams());
+        $this->assertEquals('value', $stringRoute->getParams()['param']);
+
+        $this->assertEquals($class, $intRoute->getControllerClass());
+        $this->assertNotEmpty($intRoute->getParams());
+        $this->assertEquals('123', $intRoute->getParams()['param']);
     }
 }
