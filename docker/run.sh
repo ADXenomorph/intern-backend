a75ed6a49e53b79631b6c717672f2d90d35226d5
Kremnev Sergey <kremnev@roowix.com>
2019-10-28 15:47:13 +0700

    Fixed run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+DIR=$(realpath $(dirname "$0")/..)
+
 # docker build -t internship .
 # docker stop intern && docker rm intern
 # docker build -t internship . && docker stop intern && docker rm intern && ./run.sh
-docker run -i -d -p 10080:80 -p 15432:5432 -v $0/..:/home/project --name intern internship
+docker run -i -d -p 10080:80 -p 15432:5432 -v $DIR:/home/project --name intern internship
